c5c9938bcb5f2c1e0a8543e09afcea705f0c07ca
MRyo-ie <k2085788@ie.u-ryukyu.ac.jp>
2020-08-09 21:22:53 +0900

    【リファクタリング】

--- Backend/oekaki_chat/chat/urls.py
+++ Backend/oekaki_chat/chat/urls.py
@@ -1,9 +1,13 @@
 # chat/urls.py
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+
+    # サーバー通信系
+    # path('send_message', views.send_message, name='send_message'),    
 ]
